d841099fdc8f4953ee031892d377fd123b7e1c51
Kamily Reis <matheusavakin844@gmail.com>
2025-07-28 01:39:47 -0300

    Lista de Tarefas

---
+++ README.md
@@ -0,0 +1,33 @@
+# 📝 ToDoList - Aplicação Web de Tarefas
+
+Este é um site/aplicação web ToDoList criado para ajudar na organização de tarefas diárias. Com uma interface simples e intuitiva, é possível adicionar, marcar como concluída e remover tarefas facilmente.
+
+## 🚀 Tecnologias utilizadas
+
+- HTML5
+- Tailwind CSS
+- JavaScript
+
+## 🎯 Funcionalidades
+
+- Adicionar tarefas
+- Marcar tarefas como concluídas
+- Remover tarefas
+- Interface responsiva
+
+
+## 💡 Objetivo
+
+Este projeto foi desenvolvido com o intuito de praticar e aprimorar habilidades em desenvolvimento web front-end, especialmente na manipulação do DOM com JavaScript.
+
+
+## 🔗 Acesse o site
+
+[🔗 Clique aqui para ver o projeto online]()  
+
+
+## 📁 Como usar localmente
+
+1. Clone o repositório:
+```bash
+git clone https://github.com/kamily327/to-do-list.git
---
+++ script.js
@@ -0,0 +1,50 @@
+const entrada = document.querySelector(".inputEntrada");
+const msgRes = document.querySelector(".content-tarefa");
+const btn = document.querySelector(".btn");
+const limparTarefa = document.querySelector(".btn-limpar");
+const body = document.querySelector("body");
+
+btn.addEventListener("click", function () {
+  if (entrada.value !== "") {
+    const itemTarefa = document.createElement("div");
+    itemTarefa.innerHTML = `
+
+<div class="w-full px-3 mt-[20px] mb-[0px]">
+    <div class="containerTarefa p-[20px] flex justify-between items-center mx-auto rounded-lg max-w-[1000px] bg-[#222] hover:bg-[#22222286] gap-1 transition duration-300 ease-in-out">
+        <p class="tarefa text-[1.1rem]  ">${entrada.value}</p> 
+        <div class ="botoes">
+ 
+        <button class="btn-excluir text-[1.1rem] text-[rgb(173,36,36)] px-[6px] cursor-pointer"><i class="fas fa-trash"></i></button>
+        <button class="tarefaConcluida text-[1.3rem] text-[rgb(40,119,40)] cursor-pointer"><i class="fas fa-check"></i></button>
+  
+        </div>
+           </div>
+   
+</div>`;
+
+    body.appendChild(itemTarefa);
+    const excluir = itemTarefa.querySelector(".btn-excluir");
+    const botaoConcluir = itemTarefa.querySelector(".tarefaConcluida");
+    const tarefaValue = itemTarefa.querySelector(".tarefa");
+    excluir.addEventListener("click", function () {
+      itemTarefa.remove();
+    });
+
+    botaoConcluir.addEventListener("click", function () {
+      tarefaValue.style.textDecoration = "line-through";
+      itemTarefa.querySelector(".containerTarefa").style.background =
+        "#083f0f9f";
+    });
+
+    entrada.value = "";
+    entrada.focus();
+  }
+});
+
+limparTarefa.addEventListener("click", function () {
+  const tarefas = document.querySelectorAll(".containerTarefa");
+  if (tarefas) {
+    tarefas.forEach((tarefa) => tarefa.remove());
+    entrada.value = "";
+  }
+});
